007b22c7eb48dc1af6162c22df9f835938fd189e
filmscore88 <cmtmorales24@gmail.com>
2019-08-05 12:56:18 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from './App';
 import manageTodo from './reducers/manageTodo';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { connect } from 'react-redux';
+
 
 let store = createStore(manageTodo);
 
